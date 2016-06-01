9a9025dca66075b2ec7629b23bdbf4c9272f0cb0
straysheep3 <getloose.kage@gmail.com>
2016-06-02 01:08:35 +0900

    root page if user_signed_in?

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :notes
+  authenticated :user do
+    root "notes#index", as: "authenticated_root"
+  end
   get 'welcome/index'
 
   root "welcome#index"
